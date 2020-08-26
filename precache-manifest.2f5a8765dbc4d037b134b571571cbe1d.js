2fa7a786d90d4fd9704b397496d1746d923c1b38
grJDh <rusmukhametov@gmail.com>
2020-08-26 19:22:38 +0300

    Updates

---
+++ precache-manifest.2f5a8765dbc4d037b134b571571cbe1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db8848d9868e996d2784f648164163b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4547abaf0db0da210ef7",
+    "url": "/static/css/main.8d5b6a38.chunk.css"
+  },
+  {
+    "revision": "3cf2de3ffb74e4fd958f",
+    "url": "/static/js/2.6076b98f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6076b98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4547abaf0db0da210ef7",
+    "url": "/static/js/main.2b9cc781.chunk.js"
+  },
+  {
+    "revision": "abda550f4987e395cffe",
+    "url": "/static/js/runtime-main.3ccd80de.js"
+  }
+]);
