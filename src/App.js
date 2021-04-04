2e295d85d8a25e23270daa1c504b79a659fe7a8d
grJDh <rusmukhametov@gmail.com>
2021-04-04 17:39:05 +0300

    Removed warning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import CardList from './pages/CardList/CardList';
 import AlbumList from './pages/AlbumList/AlbumList';
@@ -43,8 +43,5 @@ const App = () => {
 }
 
 export default App;
-
-// router problem (https://grjdh.github.io/campaign-characters/albums/0)
  
 // other albums
-// code review
